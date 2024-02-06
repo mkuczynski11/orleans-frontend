bc119f2c8e27c0be0114e115552c7686f5b09f52
mkuczynski11 <mkuczynski11.kontakt@wp.pl>
2024-02-06 16:30:43 +0100

    home page refreshed

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { KeycloakService } from 'keycloak-angular';
-import { KeycloakProfile } from 'keycloak-js';
 
 @Component({
   selector: 'app-home',
@@ -9,15 +8,14 @@ import { KeycloakProfile } from 'keycloak-js';
 })
 export class HomeComponent implements OnInit {
   public isLoggedIn = false;
-  public userProfile: KeycloakProfile | null = null;
 
   constructor(private readonly keycloak: KeycloakService) { }
 
   public async ngOnInit() {
     this.isLoggedIn = await this.keycloak.isLoggedIn();
+  }
 
-    if (this.isLoggedIn) {
-      this.userProfile = await this.keycloak.loadUserProfile();
-    }
+  public register() {
+    this.keycloak.register();
   }
 }
